0a490f9e541a018e69bf3d0a93be967279ea15f7
Salty.Vikings <developers@saltyvikings.com.au>
2018-08-27 13:11:00 +0800

    final pre-redux files

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,19 +8,18 @@ class SearchBar extends Component {
     }
     render() {
         return (
-            <div>
+            <div className="search-bar">
                 <input
                     value={this.state.term}
-                    onChange= {event => this.setState({term: event.target.value})}
+                    onChange={event => this.onInputChange(event.target.value)}
                 />
             </div>
         );
     }
 
-    // Can be used if fat arrow function is not used above
-
-    onInputChange(event) {
-        console.log(event.target.value);
+    onInputChange(term) {
+        this.setState({term});
+        this.props.onSearchTermChange(term);
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -14,24 +15,30 @@ const API_KEY = 'AIzaSyAvwCKvOSYt1Jx2iXlYc_tRmW5Ikj0UYYQ';
 class App extends Component  {
     constructor(props) {
 
-        YTSearch({key: API_KEY, term: 'Andy Irons'}, (videos) => {
-            this.setState({ 
-                videos: videos,
-                selectedVideo: videos[0]
-            });
-        });
-
         super(props);
 
         this.state = {
             videos: [],
             selectedVideo: null
         };
+
+        this.videoSearch('Andy Irons');
+    }
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+
+        });
     }
     render () {
+
+        const videoSearch = _.debounce( term => {this.videoSearch(term)}, 300);
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={videoSearch}/>
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList 
                     onVideoSelect={selectedVideo => this.setState({selectedVideo})}
